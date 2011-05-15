561b59ef06798a1a9ed8c9e110b9b5923536ae94
Denis <denis.labaye@gmail.com>
2011-05-15 16:05:03 +0200

    removed clojure-twitter, initialzied twitter4j

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject twitalyse "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.twitter4j/twitter4j-core "[2.1,)"]]
   :dev-dependencies [[appengine-magic "0.4.1"]])
--- twitter-poc/test/twitter_poc/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns twitter-poc.test.core
-  (:use [twitter-poc.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
