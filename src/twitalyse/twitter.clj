f7d224216068213c02c228fe5dcb6f78d2f29f44
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-04-11 18:38:48 +0200

    Reformating + add docstring.

--- src/twitalyse/twitter.clj
+++ src/twitalyse/twitter.clj
@@ -4,38 +4,45 @@
 
 (defn group-by-count
   "Takes a seq and return a map of {values, count}"
-  [s] (reduce #(assoc %1 %2 (if-let [cnt (%1 %2)]
-                              (inc cnt)
-                              1))
-              {}
-              s))
+  [s]
+  (reduce
+   #(assoc %1 %2
+           (if-let [cnt (%1 %2)]
+             (inc cnt)
+             1))
+   {}
+   s))
 
 (fact "group-by-count"
   (group-by-count ["a" "a" "b" "a"]) => {"a" 3
                                          "b" 1})
 
 (defn make-query
-  [hashtag pagenumber] (doto (Query. (str "#" hashtag))
-                         (.setRpp 100)
-                         (.setPage pagenumber)))
+  "Build the query for a hashtag and a page number"
+  [hashtag pagenumber]
+  (doto (Query. (str "#" hashtag))
+    (.setRpp 100)
+    (.setPage pagenumber)))
 
-;; raw results from twitter
 (defn raw-results
-  [hashtag pagenumber] (.search (.getInstance (TwitterFactory.))
-                                (make-query hashtag pagenumber)))
+  "Raw results from twitter"
+  [hashtag pagenumber]
+  (.search (.getInstance (TwitterFactory.))
+           (make-query hashtag pagenumber)))
 
-
-
-;; reworked results, so we have a count of the tweets by user
-(defn results-page [hashtag pagenumber]
+(defn results-page
+  "Reworked results, so we have a count of tweets by user"
+  [hashtag pagenumber]
   (map #(.getFromUser %)
        (.getTweets (raw-results hashtag pagenumber))))
 
 (defn results
-  [hashtag] (flatten (take-while seq
-                                 (map #(results-page hashtag %)
-                                      (iterate inc 1)))))
-
+  "Given a hashtag, return the seq list of pair [user count-tweets-hashtag]"
+  [hashtag]
+  (flatten
+   (take-while seq
+               (map #(results-page hashtag %)
+                    (iterate inc 1)))))
 
 ;; use to play with the repl
 '(ns user
